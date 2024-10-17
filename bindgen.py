30d066287e89bc54492a1e80251a6d7a7f74a754
Victor Gaydov <victor@enise.org>
2024-10-17 14:34:54 +0700

    Enable typeguard

--- bindgen.py
+++ bindgen.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 
-from lib import __main__
+import typeguard
+typeguard.install_import_hook('lib')
 
+from lib import __main__
 if __name__ == '__main__':
     __main__.main()
